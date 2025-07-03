d0e4c95205ab24b2cf555542780574514033279c
0suyog <dahalsunischit@gmail.com>
2025-07-03 23:18:15 +0545

    Implemented hash-object

--- app/helpers.js
+++ app/helpers.js
@@ -1,2 +1,82 @@
 const fs = require("fs");
+const crypto = require("crypto");
 const path = require("path");
+const zlib = require("node:zlib");
+
+function hash_object(data, write) {
+	let type = "blob";
+	let buffer = Buffer.from(data);
+	let size = Buffer.byteLength(buffer, "ascii");
+	const header = `${type} ${size}\0`;
+	const sha1 = crypto
+		.createHash("sha1")
+		.update(`${header}${data}`)
+		.digest("hex");
+	if (write) {
+		const compressedData = zlib.deflateSync(`${header}${data}`);
+		addObject(sha1, compressedData);
+	}
+	process.stdout(sha1);
+}
+
+function addObject(sha1, compressedData) {
+	const objectPath = path.join(
+		process.cwd(),
+		`.git/objects/${sha1.slice(0, 2)}/`
+	);
+
+	fs.mkdirSync(objectPath, { recursive: true });
+	fs.writeFileSync(path.join(objectPath, sha1.slice(2)), compressedData);
+}
+
+function hash_object(data, write) {
+	let type = "blob";
+	let buffer = Buffer.from(data);
+	let size = Buffer.byteLength(buffer, "ascii");
+	const header = `${type} ${size}\0`;
+	const sha1 = crypto
+		.createHash("sha1")
+		.update(`${header}${data}`)
+		.digest("hex");
+	const compressedData = zlib.deflateSync(`${header}${data}`);
+	if (write) {
+		addObject(sha1, compressedData);
+	}
+	console.log(sha1);
+}
+
+function read_blob(hash, arg) {
+	if (!hash) {
+		throw new Error("Expected a hash value");
+	}
+	if (!arg) {
+		throw new Error("cat-file expects two arguments");
+	}
+	const objectPath = path.join(
+		process.cwd(),
+		`.git/objects/${hash.slice(0, 2)}/${hash.slice(2)}`
+	);
+	let compressedData = fs.readFileSync(objectPath);
+	let unzippedFile = zlib.unzipSync(compressedData);
+	let fileData = unzippedFile.toString();
+	let header = fileData.split("\0")[0];
+	let length = header.length;
+	switch (arg) {
+		case "-p":
+			let content = fileData.slice(length + 1).trimEnd();
+			process.stdout.write(content);
+			break;
+		case "-s":
+			let size = header.split(" ")[1];
+			process.stdout.write(size);
+			break;
+		case "-t":
+			let type = header.split(" ")[0];
+			process.stdout.write(type);
+			break;
+		default:
+			throw new Error(`cat file doesn't recognize ${arg}`);
+	}
+}
+
+module.exports = { hash_object, read_blob };
--- app/main.js
+++ app/main.js
@@ -1,21 +1,46 @@
 const fs = require("fs");
 const path = require("path");
-const zlib = require("node:zlib");
+const { read_blob, hash_object } = require("./helpers.js");
+const readline = require("node:readline");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.error("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const command = process.argv[2];
-const arg = process.argv[3];
-const hash = process.argv[4];
+const args = process.argv.slice(3);
 
 switch (command) {
 	case "init":
 		createGitDirectory();
 		break;
 	case "cat-file":
+		let arg = args.find((arg) => arg.startsWith("-"));
+		let hash = args.find((arg) => !arg.startsWith("-"));
 		read_blob(hash, arg);
 		break;
+	case "hash-object":
+		let write = args.some((arg) => arg === "-w");
+		let stdin = args.some((arg) => arg === "--stdin");
+		let file = args.find((arg) => !arg.startsWith("-"));
+		let data;
+		if (stdin) {
+			let rl = readline.createInterface({
+				input: process.stdin,
+				output: process.stdout,
+			});
+			rl.question("", (inp) => {
+				if (inp !== null) {
+					data = inp;
+					rl.close();
+					hash_object(data, write);
+				}
+			});
+		} else {
+			data = fs.readFileSync(path.join(process.cwd(), file), "utf-8");
+			hash_object(data, write);
+		}
+		break;
+
 	default:
 		throw new Error(`Unknown command ${command}`);
 }
@@ -31,38 +56,4 @@ function createGitDirectory() {
 		"ref: refs/heads/main\n"
 	);
 	console.log("Initialized git directory");
-}
-
-function read_blob(hash, arg) {
-	if (!hash) {
-		throw new Error("Expected a hash value");
-	}
-	if (!arg) {
-		throw new Error("cat-file expects two arguments");
-	}
-	const objectPath = path.join(
-		process.cwd(),
-		`.git/objects/${hash.slice(0, 2)}/${hash.slice(2)}`
-	);
-	let compressedData = fs.readFileSync(objectPath);
-	let unzippedFile = zlib.unzipSync(compressedData);
-	let fileData = unzippedFile.toString();
-	let header = fileData.split("\0")[0];
-	let length = header.length;
-	switch (arg) {
-		case "-p":
-			let content = fileData.slice(length + 1).trimEnd();
-			process.stdout.write(content);
-			break;
-		case "-s":
-			let size = header.split(" ")[1];
-			process.stdout.write(size);
-			break;
-		case "-t":
-			let type = header.split(" ")[0];
-			process.stdout.write(type);
-			break;
-		default:
-			throw new Error(`cat file doesn't recognize ${arg}`);
-	}
 }
